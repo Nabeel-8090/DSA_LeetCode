5431bb2b54b745188a7b962ec7a852b1d4283ef8
Nabeel-8090 <sheikhnabeel2201182@gmail.com>
2025-12-06 17:12:11 +0500

    LC-1 Two Sum

---
+++ Arrays/001_Two_Sum.cpp
@@ -0,0 +1,32 @@
+// Problem: Two Sum
+// Difficulty: Easy
+// Topic: Array
+// LeetCode Link: https://leetcode.com/problems/two-sum/
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    for(int i=0; i<nums.size(); i++) {
+        for(int j=i+1; j<nums.size(); j++) {
+            if(nums[i]+nums[j] == target) {
+                cout << "[" << i << ", " << j << "]" << endl;
+                return {i, j};
+            }
+        }
+    }
+    return {};
+}
+
+int main() {
+
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 26;
+
+    twoSum(nums, target);
+
+    return 0;
+}
