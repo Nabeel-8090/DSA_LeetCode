5a4945c7cc7d9fa7a2fadfa4f80428d85f5104d0
Nabeel-8090 <sheikhnabeel2201182@gmail.com>
2025-12-22 00:10:59 +0500

    LC-217 Contains Duplicate (Sorting)

--- Arrays/217_Contains_Duplicate_Sorting.cpp
+++ Arrays/217_Contains_Duplicate_Sorting.cpp
@@ -12,8 +12,8 @@
 using namespace std;
 
 bool containsDuplicate(vector<int>& nums) {
-   sort(nums.begin(), nums.end());
-    for(int i=0; i<nums.size(); i++) {
+    sort(nums.begin(), nums.end());
+    for(int i=0; i<nums.size()-1; i++) {
         if(nums[i] == nums[i+1]) {
             return true;
         }
